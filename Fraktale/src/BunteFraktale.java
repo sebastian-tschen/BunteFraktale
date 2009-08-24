b24e76b15ead7a9aa3c5d941d85d09591ab6bdc1
sebastian <sebastian@e87c1061-dcec-4b7c-99ea-fe93ba215024>
2009-08-24 18:34:38 +0000

    url fuer subclispe

---
+++ Fraktale/src/BunteFraktale.java
@@ -0,0 +1,16 @@
+
+public class BunteFraktale {
+
+	/**
+	 * @param args 
+	 * 
+	 * 
+	 * 
+	 * subclipse: http://subclipse.tigris.org/update_1.6.x
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
